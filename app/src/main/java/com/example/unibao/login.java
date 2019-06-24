d061e10613714cb0e1b063cb2ffc79a353760083
JohnPaulVal <49104361+JohnPaulVal@users.noreply.github.com>
2019-06-23 22:47:24 -0400

    ImageButton Listos

--- app/src/main/java/com/example/unibao/login.java
+++ app/src/main/java/com/example/unibao/login.java
@@ -1,6 +1,7 @@
 package com.example.unibao;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,4 +20,25 @@ public class login extends AppCompatActivity {
         startActivity(intent);
 
     }
+    public void refresh(View view){
+
+        Intent intent = new Intent(login.this , MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void salir(View view){
+        moveTaskToBack(true);
+
+    }
+
+    public void pagina(View view){
+        Uri uri = Uri.parse("https://enlace.univalle.edu/san/webform/PAutenticar.aspx");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+
+
+
 }
